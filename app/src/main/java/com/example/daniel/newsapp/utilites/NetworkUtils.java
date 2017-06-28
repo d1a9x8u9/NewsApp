2f349aea8f4c8b2dc48a055542b53e5eb3e001b6
d1a9x8u9 <daxu7803@gmail.com>
2017-06-28 14:42:13 -0700

    Resized Text and Margins

--- app/src/main/java/com/example/daniel/newsapp/utilites/NetworkUtils.java
+++ app/src/main/java/com/example/daniel/newsapp/utilites/NetworkUtils.java
@@ -3,11 +3,16 @@ package com.example.daniel.newsapp.utilites;
 import android.net.Uri;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -20,15 +25,17 @@ public final class NetworkUtils {
             "https://newsapi.org/v1/articles";
     public static final String NEWS_BASE_URL = STATIC_NEWS_URL;
 
+    // TODO insert apikey below
     public static final String apikey = "6e6d27fa70c44a0da70427d2682402d6";
     public static final String sort = "latest";
+    public static final String source = "the-next-web";
 
     public static String PARAM_API_KEY = "apiKey";
     public static String PARAM_SORT =  "sortBy";
     public static String PARAM_SOURCE = "source";
 
 
-    public static URL buildUrl(String source){
+    public static URL buildUrl(){
         Uri buildUri = Uri.parse(NEWS_BASE_URL).buildUpon()
                 .appendQueryParameter(PARAM_SOURCE, source)
                 .appendQueryParameter(PARAM_SORT, sort)
@@ -63,4 +70,29 @@ public final class NetworkUtils {
             urlConnection.disconnect();
         }
     }
+
+
+    public static ArrayList<Repository> parseJSON(String json) throws JSONException {
+        ArrayList<Repository> result = new ArrayList<>();
+        JSONObject main = new JSONObject(json);
+        JSONArray articles = main.getJSONArray("articles");
+
+        for(int i = 0; i < articles.length(); i++){
+            JSONObject article = articles.getJSONObject(i);
+            String author = article.getString("author");
+            String title =  article.getString("title");
+            String description = article.getString("description");
+            String url_string = article.getString("url");
+            String urlToImage = article.getString("urlToImage");
+            String publishedAt = article.getString("publishedAt");
+            Repository repo = new Repository(author, title, description, url_string, urlToImage, publishedAt);
+            result.add(repo);
+
+            Log.d(TAG, "Added articles to Repo array");
+
+        }
+        return result;
+    }
+
+
 }
